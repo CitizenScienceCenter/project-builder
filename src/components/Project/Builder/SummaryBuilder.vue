efdbbee530544f5969d498bff31d8d1d0c03108e
Marcel Tanner <marcel.tanner@uzh.ch>
2019-11-19 11:59:18 +0100

    builder summary styling wip

--- src/components/Project/Builder/SummaryBuilder.vue
+++ src/components/Project/Builder/SummaryBuilder.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
+
+    <app-content-section>
+      <div class="content-subsection">
+        <div class="content-wrapper">
+          <div class="row row-centered">
+            <div class="col col-large-8 col-xlarge-6 scroll-effect">
+              <h2 class="heading centered">Congratulations</h2>
+              <p class="button-group centered">You just created your project {{ selectedProject.name }}.</p>
+              <p class="button-group centered">Want to keep going? Set it up right now!</p>
+              
+              <div class="margin-bottom">
+                <div class="button-group centered">
+                  <router-link class="button button-primary" :to="{ name: 'task.builder.material', params: { id: selectedProject.id } }">Set up</router-link>
+                </div>
+              </div>
+
+              <p class="button-group centered">
+                You can always leave it for later and visit your <router-link :to="{ name: 'project', params: { id: selectedProject.id } }">projects's page</router-link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </app-content-section>
+
+
+
     <b-row>
       <b-col>
         <h2 class="mt-3 mb-3">Congratulations</h2>
@@ -15,8 +42,13 @@
 <script>
 import { mapState } from 'vuex'
 
+import ContentSection from '@/components/shared/ContentSection.vue';
+
 export default {
   name: 'SummaryBuilder',
+  components: {
+    'app-content-section': ContentSection
+  },
   computed: {
     ...mapState('project', [
       'selectedProject'
