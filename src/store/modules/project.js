2ae50d36737470710354403bf302ef4ddbbb26df
encima <chris@gwillia.ms>
2019-11-19 14:30:09 +0100

    upload url fixed

--- src/store/modules/project.js
+++ src/store/modules/project.js
@@ -317,7 +317,7 @@ const actions = {
    * @return {Promise<any> | Thenable<any> | * | PromiseLike<T | never> | Promise<T | never>}
    */
   uploadAvatar ({ commit, state, dispatch }, { project, imageName, image }) {
-    return dispatch('c3s/media/getPresigned', ['builder/' + project, imageName], { root: true }).then(response => {
+    return dispatch('c3s/media/getPresigned', ['builder', project + '/' + imageName], { root: true }).then(response => {
       if (response) {
         return dispatch('c3s/media/upload', [response.body.msg.url, image], { root: true }).then(resp => {
           // TODO save image path to media table
